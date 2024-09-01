e63771c2e4f6f753b6a58a86c18489fe6fc8fd4a
mukundbondre <msbondre2001@gmail.com>
2024-09-02 02:03:56 +0530

    home done

--- frontend/src/containt/home/Home.js
+++ frontend/src/containt/home/Home.js
@@ -1,10 +1,47 @@
 import React from 'react';
-// import "./home.css";
+import "./home.css";
+import img from '../../images/mukund.png';
 
 function Home() {
   return (
     <div className='home'>
-        Home
+      <div className='detail'>
+        <table>
+          <p>WEB DEVELOPER</p>
+          <tr>
+            <td colSpan={2}>
+              <hr />
+            </td>
+          </tr>
+          <tr>
+            <th>NAME</th>
+            <td>MUKUND BONDRE</td>
+          </tr>
+          <tr>
+            <th>D.O.B</th>
+            <td>12 OCT 2001</td>
+          </tr>
+          <tr>
+            <th>PHONE</th>
+            <td>+91 96654 88242</td>
+          </tr>
+          <tr>
+            <th>ADDRESS</th>
+            <td>PUNE, MAHARASHTRA</td>
+          </tr>
+          <tr>
+            <th>E-MAIL</th>
+            <td>mukundbondre41@gmail.com</td>
+          </tr>
+          <tr>
+            <th>WEBSITE</th>
+            <td>www.mukundbondre.com</td>
+          </tr>
+        </table>
+      </div>
+      <div className='photo'>
+        <img src={img}/>
+      </div>
     </div>
   )
 }
