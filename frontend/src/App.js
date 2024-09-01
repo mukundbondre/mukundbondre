6550f005c8bb060053d9d3469291aea15181eb69
mukundbondre <msbondre2001@gmail.com>
2024-09-01 22:51:21 +0530

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import Home from "./containt/home/Home";
+import Navbar from "./containt/navbar/Navbar";
+
+function App() {
+  return (
+    <div className="container">
+      <Navbar/>
+      <Home />
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/containt/navbar/Navbar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import "./navbar.css";
+
+function Navbar() {
+  return (
+    <div className='navbar'>
+        <p>MUKUND</p>
+        <ul>
+            <li>HOME</li>
+            <li>ABOUT ME</li>
+            <li>SKILLS</li>
+            <li>RESUME</li>
+            <li>CONTACT</li>
+        </ul>
+    </div>
+  )
+}
+
+export default Navbar
